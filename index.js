89c04fde8a5bbb4ae3d74cfe195fd4db2358f168
root <root@burgundy.io>
2014-11-16 09:12:56 -0500

    wrote some JS; looks like it's all the JS I need?

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('yo');
+$(document).ready(function() {
+
+var loaded = [4];
+var intervalVar;
+
+function hmming() {
+  if (loaded[0] > 0) {
+    $('#suggestion')[0].textContent = 'hmm'+['','.','..','...'][4-loaded[0]];
+    loaded[0] = loaded[0] - 1;
+  }
+  else {
+    clearInterval(intervalVar);
+  }
+}
+
+intervalVar = window.setInterval(function() {hmming()}, 150);
+
+$.get('http://burgundy.io:8080/', function(data) {
+  loaded[0] = 0;
+  $('#suggestion')[0].textContent = data;
+});
+
+});
