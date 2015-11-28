0cb9841789368afb7505a53036d931c46bac73e1
Shariq Hashme <shariqhashme@gmail.com>
2015-11-28 10:34:57 -0500

    filtering out short words and c words

--- rnn/rnnserver.py
+++ rnn/rnnserver.py
@@ -8,7 +8,7 @@ import random
 Q = remotequeue.make('secret', False)
 amazing_models = open('amazing_models').read().splitlines()
 
-def sample_model(model, chars = 1000):
+def sample_model(model, chars = 5000):
     try:
         model_path = model.split(';')[0].strip()
         temp = model.split(';')[1].strip()
@@ -26,15 +26,23 @@ def create_words(num):
         model = random.choice(amazing_models)
         print 'sampling from model',model
         for word in sample_model(model):
-            if len(word) >= 3:
-                pool.add(word)
+            if 12 > len(word) > 3:
+                if random.random() < len(word)/8:
+                    if word[0] == 'c':
+                        if random.random() > 0.2:
+                            continue
+                    if word[0] == 'b':
+                        if random.random() > 0.5:
+                            continue
+                    pool.add(word)
     print 'created that many words! now sorting by pronunciation score...'
     words = sorted(pool, key = pronounce.score)
     return words[-num:]
 
 while True:
     while Q.qsize() < 2500:
-        words = create_words(100)
+        print 'we have only',Q.qsize(),'words'
+        words = create_words(250)
         for word in words:
             Q.put(word)
     time.sleep(0.25)
